aee78698dbb29033c42f9716ab8615d60db6d5c5
Bodhaansh Ravipati <br183@duke.edu>
2024-02-19 18:37:03 -0500

    Complete my API documentation

--- doc/plan/api/ViewInternal.java
+++ doc/plan/api/ViewInternal.java
@@ -1,19 +1,72 @@
 public interface ViewInternal {
 
-  //overridden
+  /*
+   * Overrides a method from higher in the inheritance
+   * tree. Handles the user creating a custom variable.
+   */
   public void onUpdateVariable(String variableName, double value);
+
+  /*
+   * Overrides a method from higher in the inheritance
+   * tree. Handles the user changing the turtle's position.
+   */
   public void onUpdateTurtleState(ImmutableTurtle turtle);
+
+  /*
+   * Overrides a method from higher in the inheritance
+   * tree. Handles the user changing the value of a
+   * previously defined variable.
+   */
   public void onUpdateValue(String text, double value);
 
+  /*
+   * Handles the user switching languages.
+   */
   public void setLanguage(String lang);
+
+  /*
+   * Handles the user switching background color.
+   */
   public void setColor(Color color);
 
+  /*
+   * Checks if the text field has at least
+   * one valid command in it.
+   */
   public boolean hasCommandString();
+
+  /*
+   * Returns the current text field body.
+   */
   public String getCommandString();
 
-  public void displayeErrorMessage( );
+  /*
+   * Gets the current exception, and shows
+   * it as an alert.
+   */
+  public void displayErrorMessage();
+
+  /*
+   * Gets the current settings, and shows
+   * it as a panel.
+   */
   public void displaySettingsPanel();
+
+  /*
+   * Gets the current XMl file, and shows
+   * it as an panel.
+   */
   public void displaySavePanel();
+
+  /*
+   * Gets the current documentation, and shows
+   * it as an panel.
+   */
   public void displayHelpPanel();
+
+  /*
+   * Gets the current command history, and shows
+   * it as an panel.
+   */
   public void displayHistoryPanel();
 }
