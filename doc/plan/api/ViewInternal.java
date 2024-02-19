5acd57a00dd979ed46a6eb0516fb44e2acdc4b8e
Bodhaansh Ravipati <br183@duke.edu>
2024-02-19 17:05:50 -0500

    Add View API methods

--- doc/plan/api/ViewInternal.java
+++ doc/plan/api/ViewInternal.java
@@ -1,10 +1,16 @@
 public interface ViewInternal {
 
+  //overridden
   public void onUpdateVariable(String variableName, double value);
   public void onUpdateTurtleState(ImmutableTurtle turtle);
   public void onUpdateValue(String text, double value);
+
+  public void setLanguage(String lang);
+  public void setColor(Color color);
+
   public boolean hasCommandString();
   public String getCommandString();
+
   public void displayeErrorMessage( );
   public void displaySettingsPanel();
   public void displaySavePanel();
