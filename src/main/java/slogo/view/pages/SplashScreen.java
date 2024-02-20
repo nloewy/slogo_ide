4bea5411a3c687f19c8bb0c383091f5344436305
Bodhaansh Ravipati <br183@duke.edu>
2024-02-20 12:50:55 -0500

    Add Selector

--- src/main/java/slogo/view/pages/SplashScreen.java
+++ src/main/java/slogo/view/pages/SplashScreen.java
@@ -3,61 +3,78 @@ package slogo.view.pages;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.List;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
-
 
 public class SplashScreen extends Page {
 
-  private final Group root;
+    private final Group root;
 
-//   private final Button loadFileButton = new Button(Main.getInternationalText("loadText"));
-  private final Stage stage;
+    // private final Button loadFileButton = new
+    // Button(Main.getInternationalText("loadText"));
+    private final Stage stage;
 
-  private final Image logo;
-  private final ImageView logoView;
+    private final Image logo;
+    private final ImageView logoView;
 
-  public SplashScreen(Stage stage) throws FileNotFoundException {
-    super();
-    this.stage = stage;
+    private final ComboBox langBox;
+    private final ObservableList<String> langOptions;
+    private final TilePane langPane;
 
-    logo = new Image(new FileInputStream("src/main/resources/SlogoLOGO.png"));
-    logoView = new ImageView(logo);
+    public SplashScreen(Stage stage) throws FileNotFoundException {
+        super();
+        this.stage = stage;
 
-    root = new Group();
-    root.getChildren().add(logoView);
+        logo = new Image(new FileInputStream("src/main/resources/SlogoLOGO.png"));
+        logoView = new ImageView(logo);
 
-    // root.getChildren().add(loadFileButton);
-  }
+        root = new Group();
 
-//   private void setLoadFileButtonAction() throws FileFieldException {
-//     File dataFile = Page.FILE_CHOOSER.showOpenDialog(stage);
-//     Simulation simulation = View.createSimulation(dataFile);
-//     if (simulation != null) {
-//       Main.setLanguage(simulation.getConfig().getLanguage());
-//       view.toSimulationPage(simulation, simulation.getConfig().getStateColor().getStateColorMap());
-//     }
-//   }
+        langOptions = FXCollections.observableArrayList(
+                "Option 1",
+                "Option 2",
+                "Option 3");
+        langBox = new ComboBox<String>(langOptions);
+        langPane = new TilePane(langBox);
 
-  @Override
-  public void setUp() {
-    // loadFileButton.setLayoutX(500);
-    // loadFileButton.setLayoutY(300);
-    // loadFileButton.setOnAction(event -> {
-    //   try {
-    //     setLoadFileButtonAction();
-    //   } catch (FileFieldException e) {
-    //     e.printStackTrace();
-    //   }
-    // });
-  }
+        root.getChildren().add(logoView);
+        root.getChildren().add(langPane);
+    }
 
-  @Override
-  public Group getGroup() {
-    return root;
-  }
+    // private void setLoadFileButtonAction() throws FileFieldException {
+    // File dataFile = Page.FILE_CHOOSER.showOpenDialog(stage);
+    // Simulation simulation = View.createSimulation(dataFile);
+    // if (simulation != null) {
+    // Main.setLanguage(simulation.getConfig().getLanguage());
+    // view.toSimulationPage(simulation,
+    // simulation.getConfig().getStateColor().getStateColorMap());
+    // }
+    // }
+
+    @Override
+    public void setUp() {
+        // loadFileButton.setLayoutX(500);
+        // loadFileButton.setLayoutY(300);
+        // loadFileButton.setOnAction(event -> {
+        // try {
+        // setLoadFileButtonAction();
+        // } catch (FileFieldException e) {
+        // e.printStackTrace();
+        // }
+        // });
+    }
+
+    @Override
+    public Group getGroup() {
+        return root;
+    }
 }
