12d9fd3329eaff022630087ab56d0dd28ead5a1c
Noah Loewy <noah.loewy@duke.edu>
2024-02-22 00:02:38 -0500

    Sin Cos Tan

---
+++ src/main/java/slogo/model/command/math/CosineCommand.java
@@ -0,0 +1,19 @@
+package slogo.model.command.math;
+
+import java.util.List;
+import slogo.mathutils.MathUtils;
+import slogo.model.SlogoListener;
+import slogo.model.command.Command;
+
+public class CosineCommand extends Command {
+
+
+  public double execute(List<Double> arguments) {
+    return Math.cos(MathUtils.toRadians(arguments.get(0)));
+
+  }
+  public void notifyListener(SlogoListener listener, double value) {
+    super.notifyListener(listener, value);
+  }
+
+}
