007012826e588a0255a860f410179eab4fc26ba0
Noah Loewy <noah.loewy@duke.edu>
2024-02-21 22:34:54 -0500

    BCK CMD

--- src/main/java/slogo/model/command/BackwardCommand.java
+++ src/main/java/slogo/model/command/BackwardCommand.java
@@ -4,17 +4,17 @@ import java.util.List;
 import slogo.mathutils.MathUtils;
 import slogo.model.Turtle;
 
-public class ForwardCommand extends Command {
+public class BackwardCommand extends Command {
   private Turtle myTurtle;
 
-  public ForwardCommand(Turtle turtle) {
+  public BackwardCommand(Turtle turtle) {
     myTurtle = turtle;
   }
   @Override
   public double execute(List<Double> arguments) {
     double pixels = arguments.get(0);
-    myTurtle.setX(myTurtle.getX()+ pixels * Math.sin(MathUtils.toRadians(myTurtle.getHeading())));
-    myTurtle.setY(myTurtle.getY()+ pixels * Math.cos(MathUtils.toRadians(myTurtle.getHeading())));
+    myTurtle.setX(myTurtle.getX() - pixels * Math.sin(MathUtils.toRadians(myTurtle.getHeading())));
+    myTurtle.setY(myTurtle.getY() - pixels * Math.cos(MathUtils.toRadians(myTurtle.getHeading())));
     return pixels;
   }
 }
