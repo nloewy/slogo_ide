0e38c84f3f7d2a37458b85e93648e23226b78e1d
Noah Loewy <noah.loewy@duke.edu>
2024-02-22 00:17:53 -0500

    Equal Less CMD

---
+++ src/main/java/slogo/model/command/bool/LessCommand.java
@@ -0,0 +1,19 @@
+package slogo.model.command.bool;
+
+import java.util.List;
+import slogo.mathutils.MathUtils;
+import slogo.model.SlogoListener;
+import slogo.model.command.Command;
+
+public class LessCommand extends Command {
+
+
+  public double execute(List<Double> arguments) {
+    return arguments.get(0) < arguments.get(1) ? 1 : 0;
+
+  }
+  public void notifyListener(SlogoListener listener, double value) {
+    super.notifyListener(listener, value);
+  }
+
+}
