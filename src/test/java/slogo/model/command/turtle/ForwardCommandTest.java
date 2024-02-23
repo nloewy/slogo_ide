19bdca12befd1145b5ac85be022e7a2e9d61fb6f
Noah Loewy <noah.loewy@duke.edu>
2024-02-22 23:38:51 -0500

    FWD TEST

--- src/test/java/slogo/model/command/turtle/ForwardCommandTest.java
+++ src/test/java/slogo/model/command/turtle/ForwardCommandTest.java
@@ -29,5 +29,54 @@ public class ForwardCommandTest {
     node.addChildren(new ConstantNode(dist, myTurtle));
     assertEquals(node.getValue(), 75);
     assertEquals(myTurtle.getY(), 75);
+
   }
-}
+
+  @Test
+  void testForwardWithHeading()
+      throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    Node node = new CommandNode("slogo.model.command.turtle.ForwardCommand", myTurtle);
+    myTurtle.setHeading(60);
+    String dist = "20";
+    node.addChildren(new ConstantNode(dist, myTurtle));
+    assertEquals(node.getValue(), 20);
+    assertEquals(myTurtle.getX(), 10*Math.sqrt(3), .00001);
+    assertEquals(myTurtle.getY(), 10, .00001);
+  }
+
+  @Test
+  void testForwardWithNonZeroOrigin()
+      throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    Node node = new CommandNode("slogo.model.command.turtle.ForwardCommand", myTurtle);
+    myTurtle.setHeading(60);
+    myTurtle.setX(50);
+    myTurtle.setY(70);
+    String dist = "20";
+    node.addChildren(new ConstantNode(dist, myTurtle));
+    assertEquals(node.getValue(), 20);
+    assertEquals(myTurtle.getX(), 50 + 10 * Math.sqrt(3), .00001);
+    assertEquals(myTurtle.getY(), 80, .00001);
+  }
+
+  @Test
+  void testForwardWithZeroDistance()
+      throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    Node node = new CommandNode("slogo.model.command.turtle.ForwardCommand", myTurtle);
+    String dist = "0";
+    node.addChildren(new ConstantNode(dist, myTurtle));
+    assertEquals(node.getValue(), 0);
+    assertEquals(myTurtle.getX(), 0);
+    assertEquals(myTurtle.getY(), 0);
+  }
+
+  @Test
+  void testForwardNegativeDistance()
+      throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+    Node node = new CommandNode("slogo.model.command.turtle.ForwardCommand", myTurtle);
+    String dist = "-75";
+    node.addChildren(new ConstantNode(dist, myTurtle));
+    assertEquals(node.getValue(), -75);
+    assertEquals(myTurtle.getY(), -75);
+
+  }
+  }
