02b1726a85e6a98d4fb913bcc585513254db4a69
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 09:50:15 -0500

    Ycor cmd doc

--- src/main/java/slogo/model/command/query/YcoordinateCommand.java
+++ src/main/java/slogo/model/command/query/YcoordinateCommand.java
@@ -7,15 +7,39 @@ import slogo.model.api.SlogoListener;
 import slogo.model.command.Command;
 import slogo.model.node.Node;
 
+/**
+ * The YcoordinateCommand class represents a command that retrieves the Y coordinate of the requested turtle.
+ * It returns the Y coordinate of the requested turtle.
+ *
+ * @author Noah Loewy
+ */
 public class YcoordinateCommand implements Command {
 
+  /**
+   * The number of arguments this command requires.
+   */
   public static final int NUM_ARGS = 0;
+
   private final ModelState modelState;
 
+  /**
+   * Constructs an instance of YcoordinateCommand with the given model state and listener.
+   *
+   * @param modelState the model state
+   * @param listener the listener for state change events
+   */
   public YcoordinateCommand(ModelState modelState, SlogoListener listener) {
     this.modelState = modelState;
   }
 
+  /**
+   * Retrieves the Y coordinate of the requested turtle.
+   *
+   * @param arguments a list of nodes representing arguments (not used in this command)
+   * @return the Y coordinate of the requested turtle
+   * @throws InvocationTargetException if an error occurs during execution
+   * @throws IllegalAccessException if access is denied during execution
+   */
   @Override
   public double execute(List<Node> arguments)
       throws InvocationTargetException, IllegalAccessException {
