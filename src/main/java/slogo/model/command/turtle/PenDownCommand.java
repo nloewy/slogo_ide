cc15f7e88f424c9ed04467262740175cd22bb7d3
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 10:08:54 -0500

    PenDown Cmd Doc

--- src/main/java/slogo/model/command/turtle/PenDownCommand.java
+++ src/main/java/slogo/model/command/turtle/PenDownCommand.java
@@ -8,17 +8,42 @@ import slogo.model.api.SlogoListener;
 import slogo.model.command.Command;
 import slogo.model.node.Node;
 
+/**
+ * The PenDownCommand class represents the command to put the turtle's pen down.
+ * It sets the turtle's pen to the down position, enabling it to draw when it moves.
+ *
+ * @author Noah Loewy
+ */
 public class PenDownCommand implements Command {
 
+  /**
+   * The number of arguments this command expects.
+   */
+
   public static final int NUM_ARGS = 0;
+
   private final ModelState modelState;
   private final SlogoListener listener;
 
+  /**
+   * Constructs a PenDownCommand with the given model state and listener.
+   *
+   * @param modelState the model state containing information about the turtle
+   * @param listener the listener for state change events
+   */
   public PenDownCommand(ModelState modelState, SlogoListener listener) {
     this.modelState = modelState;
     this.listener = listener;
   }
 
+  /**
+   * Executes the PenDownCommand, putting the turtle's pen down.
+   *
+   * @param arguments a list of nodes representing the arguments for this command (empty for this command)
+   * @return 1.0 to indicate that the pen is down and successful execution
+   * @throws InvocationTargetException if an error occurs during execution
+   * @throws IllegalAccessException if access is denied during execution
+   */
   @Override
   public double execute(List<Node> arguments)
       throws InvocationTargetException, IllegalAccessException {
