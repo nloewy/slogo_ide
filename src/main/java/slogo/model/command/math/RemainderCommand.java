13a37dff580a1f17c5118d85e0b1860970f90873
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 09:40:30 -0500

    Remainder Cmd Doc

--- src/main/java/slogo/model/command/math/RemainderCommand.java
+++ src/main/java/slogo/model/command/math/RemainderCommand.java
@@ -2,26 +2,52 @@ package slogo.model.command.math;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
 import slogo.model.ModelState;
 import slogo.model.exceptions.InvalidOperandException;
 import slogo.model.api.SlogoListener;
 import slogo.model.command.Command;
 import slogo.model.node.Node;
 
+/**
+ * The RemainderCommand class represents the remainder mathematical operation.
+ * It calculates the remainder of the division of two numbers.
+ *
+ * @author Noah Loewy
+ */
 public class RemainderCommand implements Command {
 
+  /**
+   * The number of arguments this command requires.
+   */
   public static final int NUM_ARGS = 2;
 
+  /**
+   * Constructs an instance of RemainderCommand with the given model state and listener. This constructor
+   * does not actually do anything, and exists for the sake of consistency across commands.
+   *
+   * @param modelState the model state
+   * @param listener the listener for state change events
+   */
   public RemainderCommand(ModelState modelState, SlogoListener listener) {
 
   }
 
+  /**
+   * Executes the remainder mathematical operation.
+   *
+   * @param arguments a list containing two nodes representing the dividend and divisor
+   * @return the remainder of the division of the dividend by the divisor
+   * @throws InvocationTargetException if an error occurs during execution
+   * @throws IllegalAccessException if access is denied during execution
+   * @throws InvalidOperandException if the divisor is zero
+   */
   @Override
   public double execute(List<Node> arguments)
       throws InvocationTargetException, IllegalAccessException {
-    if(arguments.get(1).evaluate()==0) {
+    if (arguments.get(1).evaluate() == 0) {
       throw new InvalidOperandException("Divisor must be Non-Zero");
     }
-     return arguments.get(0).evaluate() % arguments.get(1).evaluate();
+    return arguments.get(0).evaluate() % arguments.get(1).evaluate();
   }
 }
