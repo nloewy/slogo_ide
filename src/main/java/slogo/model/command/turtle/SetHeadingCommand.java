db3200d91bffa0213e961e0864f7359d5b1cfb35
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 10:13:19 -0500

    Set Heading Cmd Doc'

--- src/main/java/slogo/model/command/turtle/SetHeadingCommand.java
+++ src/main/java/slogo/model/command/turtle/SetHeadingCommand.java
@@ -8,18 +8,41 @@ import slogo.model.api.SlogoListener;
 import slogo.model.command.Command;
 import slogo.model.node.Node;
 
+/**
+ * The SetHeadingCommand class represents the command to set the turtle's heading to a specified angle.
+ * It sets the turtle's heading to the given angle and calculates the minimum angle of rotation to reach the new heading.
+ *
+ * @author Noah Loewy
+ */
 public class SetHeadingCommand implements Command {
 
+  /**
+   * The number of arguments this command expects.
+   */
   public static final int NUM_ARGS = 1;
 
   private final ModelState modelState;
   private final SlogoListener listener;
 
+  /**
+   * Constructs a SetHeadingCommand with the given model state and listener.
+   *
+   * @param modelState the model state containing information about the turtle
+   * @param listener the listener for state change events
+   */
   public SetHeadingCommand(ModelState modelState, SlogoListener listener) {
     this.modelState = modelState;
     this.listener = listener;
   }
 
+  /**
+   * Executes the SetHeadingCommand, setting the turtle's heading to the specified angle.
+   *
+   * @param arguments a list of nodes representing the arguments for this command (containing one node with the angle to set)
+   * @return the minimum angle of rotation needed to reach the new heading
+   * @throws InvocationTargetException if an error occurs during execution
+   * @throws IllegalAccessException if access is denied during execution
+   */
   @Override
   public double execute(List<Node> arguments)
       throws InvocationTargetException, IllegalAccessException {
