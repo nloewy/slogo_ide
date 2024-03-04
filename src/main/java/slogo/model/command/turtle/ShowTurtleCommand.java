5be56daca68342e5e0e63a7850652f1ca73d2f17
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 10:15:11 -0500

    Show Turtle Cmd Doc

--- src/main/java/slogo/model/command/turtle/ShowTurtleCommand.java
+++ src/main/java/slogo/model/command/turtle/ShowTurtleCommand.java
@@ -8,16 +8,46 @@ import slogo.model.api.SlogoListener;
 import slogo.model.command.Command;
 import slogo.model.node.Node;
 
+/**
+ * The ShowTurtleCommand class represents the command to show the turtle.
+ * It sets the visibility of the turtle to true.
+ * Showing the turtle means it will be displayed on the screen.
+ * This command does not take any arguments.
+ *
+ * @author Noah Loewy
+ */
+
 public class ShowTurtleCommand implements Command {
+
+  /**
+   * The number of arguments this command requires.
+   */
 
   public static final int NUM_ARGS = 0;
   private final ModelState modelState;
   private final SlogoListener listener;
 
+  /**
+   * Constructs an instance of ShowTurtleCommand with the given model state and listener.
+   *
+   * @param modelState the model state
+   * @param listener   the listener for state change events
+   */
+
   public ShowTurtleCommand(ModelState modelState, SlogoListener listener) {
     this.modelState = modelState;
     this.listener = listener;
   }
+
+  /**
+   * Executes the command to show the turtle.
+   * Sets the visibility of the turtle to true.
+   *
+   * @param arguments a list containing nodes representing the arguments (none required)
+   * @return 1.0 indicating successful execution
+   * @throws InvocationTargetException if an error occurs during execution
+   * @throws IllegalAccessException    if access is denied during execution
+   */
 
   @Override
   public double execute(List<Node> arguments)
@@ -28,3 +58,5 @@ public class ShowTurtleCommand implements Command {
     return 1.0;
   }
 }
+
+
