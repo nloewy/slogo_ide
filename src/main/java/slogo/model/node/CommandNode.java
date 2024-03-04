4c1dadf1b48597021cc2d90e30b6c55e49640be2
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 11:59:17 -0500

    CMD NODE doc

--- src/main/java/slogo/model/node/CommandNode.java
+++ src/main/java/slogo/model/node/CommandNode.java
@@ -11,14 +11,34 @@ import slogo.model.exceptions.InvalidOperandException;
 import slogo.model.api.SlogoListener;
 import slogo.model.command.Command;
 
+/**
+ * The CommandNode class represents a node that executes a command in the Slogo language.
+ *
+ * This class adheres to the Factory Pattern, as it dynamically creates instances of the Command
+ * class based on the token stored in the CommandNode. It dynamically loads the Command class using
+ * reflection and then instantiates it using the constructor that taken the ModelState and
+ * SlogoListener as arguments, effectively creating a new Command instance. The created Command
+ * instance is then used to execute the command operation, passing the list of children nodes as
+ * arguments. If any errors occur during the creation or execution of the Command instance,
+ * appropriate exceptions are thrown.
+ *
+ * @author Noah Loewy
+ */
+
 public class CommandNode extends Node {
 
   private static final String BASE_PACKAGE = "slogo.model.command.";
   private final String myToken;
   private final ModelState myModelState;
   private Command command;
-  private Method m;
-  private String myFullToken;
+  private Method method;
+  /**
+   * Constructs a new CommandNode with the given token and model state.
+   *
+   * @param token     the token representing the command
+   * @param modelState the model state of the application
+   * @throws ClassNotFoundException if the command class cannot be found
+   */
 
   public CommandNode(String token, ModelState modelState)
       throws ClassNotFoundException {
@@ -27,14 +47,24 @@ public class CommandNode extends Node {
     myModelState = modelState;
   }
 
+  /**
+   * Evaluates the CommandNode by dynamically invoking the execute method of the corresponding
+   * Command class using reflection based on the token. So long as the number of arguments match,
+   * the command is executed by calling the execute method on the proper Command object
+   *
+   * @return the result of executing the CommandNode
+   * @throws InvalidCommandException       if the Command class corresponding to token is not found
+   * @throws InsufficientArgumentsException if unexpected number of arguments received
+   * @throws InvalidOperandException       if an invalid operand is encountered during execution
+   */
 
   @Override
-  public double evaluate() throws InvocationTargetException, IllegalAccessException {
+  public double evaluate() throws InvalidCommandException, InsufficientArgumentsException, InvalidOperandException {
     try {
       Class<?> clazz = Class.forName(BASE_PACKAGE + myToken + "Command");
       command = (Command) clazz.getDeclaredConstructor(ModelState.class, SlogoListener.class)
           .newInstance(myModelState, getListener());
-      m = clazz.getDeclaredMethod("execute", List.class);
+      method = clazz.getDeclaredMethod("execute", List.class);
     } catch (ClassNotFoundException | InvocationTargetException | InstantiationException |
              NoSuchMethodException | IllegalAccessException e) {
       throw new InvalidCommandException(
@@ -45,7 +75,7 @@ public class CommandNode extends Node {
       throw new InsufficientArgumentsException("", getToken());
     }
     try {
-      return (double) m.invoke(command, children);
+      return (double) method.invoke(command, children);
 
     } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
       Class<? extends RuntimeException> causeClass = (Class<? extends RuntimeException>) e.getCause()
@@ -57,6 +87,15 @@ public class CommandNode extends Node {
     }
   }
 
+/**
+ * Retrieves the number of arguments expected for the Command corresponding to this CommandNode.
+ * This method dynamically loads the Command class using reflection based on the token,
+ * and then attempts to retrieve the NUM_ARGS field.
+ *
+ * @return the number of arguments expected for the Command
+ * @throws IncompleteClassException if the NUM_ARGS field is not found for the Command class
+ */
+
   public int getNumArgs() throws IncompleteClassException {
     try {
       return (int) Class.forName(BASE_PACKAGE + myToken + "Command").getField("NUM_ARGS").get(null);
@@ -65,6 +104,12 @@ public class CommandNode extends Node {
           "Error getting number of arguments. Field NUM_ARGS not found for " + myToken);
     }
   }
+
+  /**
+   * Retrieves the token associated with this CommandNode.
+   *
+   * @return the token (name) of the command
+   */
 
   public String getToken() {
     return myToken;
