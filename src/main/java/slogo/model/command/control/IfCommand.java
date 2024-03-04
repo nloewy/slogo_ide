9cc9f9a9a99bdaee6ead10363f9a92b4377ecd83
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 09:20:59 -0500

    If CMD Doc

--- src/main/java/slogo/model/command/control/IfCommand.java
+++ src/main/java/slogo/model/command/control/IfCommand.java
@@ -2,17 +2,45 @@ package slogo.model.command.control;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
 import slogo.model.ModelState;
 import slogo.model.api.SlogoListener;
 import slogo.model.command.Command;
 import slogo.model.node.Node;
 
+/**
+ * The IfCommand class represents the "if" control structure.
+ *
+ * It evaluates a condition and executes a set of commands if the condition is true.
+ *
+ * @author Noah Loewy
+ */
 public class IfCommand implements Command {
 
+  /**
+   * The number of arguments this command requires.
+   */
   public static final int NUM_ARGS = 2;
 
+  /**
+   * Constructs an instance of IfCommand with the given model state and listener. This constructor
+   * does not actually do anything, and exists for the sake of consistency across commands.
+   *
+   * @param modelState the model state
+   * @param listener the listener for state change events
+   */
   public IfCommand(ModelState modelState, SlogoListener listener) {
   }
+
+/**
+ * Executes the "if" control structure.
+ *
+ * @param arguments a list containing two nodes: the first node contains the condition to evaluate,
+ *                  and the second node contains the command nodes to execute if the condition is true
+ * @return the result of the last evaluated command if the condition is true, otherwise 0.0
+ * @throws InvocationTargetException if an error occurs during execution
+ * @throws IllegalAccessException if access is denied during execution
+ */
 
   @Override
   public double execute(List<Node> arguments)
