78bd1db97290b13fcc9efa6d4c54ec1eb8f836ee
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 09:39:16 -0500

    Random Cmd Doc

--- src/main/java/slogo/model/command/math/RandomCommand.java
+++ src/main/java/slogo/model/command/math/RandomCommand.java
@@ -2,19 +2,45 @@ package slogo.model.command.math;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
 import slogo.model.ModelState;
 import slogo.model.exceptions.InvalidOperandException;
 import slogo.model.api.SlogoListener;
 import slogo.model.command.Command;
 import slogo.model.node.Node;
 
+/**
+ * The RandomCommand class represents the random mathematical operation.
+ * It generates a random number between 0 (inclusive) and a specified maximum value (exclusive).
+ *
+ * @author Noah Loewy
+ */
 public class RandomCommand implements Command {
 
+  /**
+   * The number of arguments this command requires.
+   */
   public static final int NUM_ARGS = 1;
 
+  /**
+   * Constructs an instance of RandomCommand with the given model state and listener. This constructor
+   * does not actually do anything, and exists for the sake of consistency across commands.
+   *
+   * @param modelState the model state
+   * @param listener the listener for state change events
+   */
   public RandomCommand(ModelState modelState, SlogoListener listener) {
   }
 
+  /**
+   * Executes the random mathematical operation.
+   *
+   * @param arguments a list containing a single node representing the maximum value
+   * @return a random number between 0 (inclusive) and the specified maximum value (exclusive)
+   * @throws InvocationTargetException if an error occurs during execution
+   * @throws IllegalAccessException if access is denied during execution
+   * @throws InvalidOperandException if the maximum value is negative
+   */
   @Override
   public double execute(List<Node> arguments)
       throws InvocationTargetException, IllegalAccessException {
