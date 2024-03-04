4bdc838c859e49e10cd4bea623198d2949041038
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 09:41:36 -0500

    sqrt cmd doc

--- src/main/java/slogo/model/command/math/SquareRootCommand.java
+++ src/main/java/slogo/model/command/math/SquareRootCommand.java
@@ -2,19 +2,44 @@ package slogo.model.command.math;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
 import slogo.model.ModelState;
 import slogo.model.exceptions.InvalidOperandException;
 import slogo.model.api.SlogoListener;
 import slogo.model.command.Command;
 import slogo.model.node.Node;
 
+/**
+ * The SquareRootCommand class represents the square root mathematical operation.
+ * It calculates the square root of a given number.
+ *
+ * @author Noah Loewy
+ */
 public class SquareRootCommand implements Command {
 
+  /**
+   * The number of arguments this command requires.
+   */
   public static final int NUM_ARGS = 1;
 
+  /**
+   * Constructs an instance of SquareRootCommand with the given model state and listener. This constructor
+   * does not actually do anything, and exists for the sake of consistency across commands.
+   *
+   * @param modelState the model state
+   * @param listener the listener for state change events
+   */
   public SquareRootCommand(ModelState modelState, SlogoListener listener) {
   }
 
+  /**
+   * Executes the square root mathematical operation.
+   *
+   * @param arguments a list containing a single node representing the number to calculate the square root of
+   * @return the square root of the input number
+   * @throws InvocationTargetException if an error occurs during execution
+   * @throws IllegalAccessException if access is denied during execution
+   */
   @Override
   public double execute(List<Node> arguments)
       throws InvocationTargetException, IllegalAccessException {
