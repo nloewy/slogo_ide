5a97c946270223e9e1fde31687f482c67dd90a5a
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 09:46:33 -0500

    Heading Cmd Doc

--- src/main/java/slogo/model/command/query/HeadingCommand.java
+++ src/main/java/slogo/model/command/query/HeadingCommand.java
@@ -7,16 +7,39 @@ import slogo.model.api.SlogoListener;
 import slogo.model.command.Command;
 import slogo.model.node.Node;
 
+/**
+ * The HeadingCommand class represents a command that retrieves the heading of the requested turtle.
+ * It returns the heading of the requested turtle in the model state.
+ *
+ * @author [Author Name]
+ */
 public class HeadingCommand implements Command {
 
+  /**
+   * The number of arguments this command requires.
+   */
   public static final int NUM_ARGS = 0;
+
   private final ModelState modelState;
 
+  /**
+   * Constructs an instance of HeadingCommand with the given model state and listener.
+   *
+   * @param modelState the model state
+   * @param listener the listener for state change events
+   */
   public HeadingCommand(ModelState modelState, SlogoListener listener) {
     this.modelState = modelState;
   }
 
-
+  /**
+   * Retrieves the heading of the requested turtle.
+   *
+   * @param arguments a list of nodes representing arguments (not used in this command)
+   * @return the heading of the active turtle
+   * @throws InvocationTargetException if an error occurs during execution
+   * @throws IllegalAccessException if access is denied during execution
+   */
   @Override
   public double execute(List<Node> arguments)
       throws InvocationTargetException, IllegalAccessException {
