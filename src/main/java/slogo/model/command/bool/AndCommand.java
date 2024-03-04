ead19742bf432d9887e185a76133d0d2f4a3dc2a
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 08:58:30 -0500

    And Equal Javadoc

--- src/main/java/slogo/model/command/bool/AndCommand.java
+++ src/main/java/slogo/model/command/bool/AndCommand.java
@@ -2,23 +2,47 @@ package slogo.model.command.bool;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
 import slogo.model.ModelState;
 import slogo.model.api.SlogoListener;
 import slogo.model.command.Command;
 import slogo.model.node.Node;
 
+/**
+ * The AndCommand class represents the logical AND operation.
+ * It evaluates two nodes and returns 1.0 if both are true (non-zero), otherwise returns 0.0.
+ *
+ * @author Noah Loewy
+ */
+
 public class AndCommand implements Command {
 
+  /**
+   * The number of arguments this command requires.
+   */
   public static final int NUM_ARGS = 2;
 
-
+  /**
+   * Constructs an instance of AndCommand with the given model state and listener. This constructor
+   * does not actually do anything, and exists
+   *
+   * @param modelState the model state
+   * @param listener the listener for state change events
+   */
   public AndCommand(ModelState modelState, SlogoListener listener) {
   }
 
+  /**
+   * Executes the logical AND operation on the provided boolean expressions.
+   *
+   * @param arguments a list of nodes, which can each be evaluated
+   * @return 1.0 if both expressions are non-zero, otherwise returns 0.0
+   * @throws InvocationTargetException if an error occurs during execution
+   * @throws IllegalAccessException if access is denied during execution
+   */
   @Override
   public double execute(List<Node> arguments)
       throws InvocationTargetException, IllegalAccessException {
     return (!(arguments.get(0).evaluate() == 0) && !(arguments.get(1).evaluate() == 0)) ? 1.0 : 0.0;
-
   }
 }
--- src/main/java/slogo/model/command/bool/EqualCommand.java
+++ src/main/java/slogo/model/command/bool/EqualCommand.java
@@ -2,18 +2,43 @@ package slogo.model.command.bool;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+
 import slogo.model.ModelState;
 import slogo.model.api.SlogoListener;
 import slogo.model.command.Command;
 import slogo.model.node.Node;
 
+/**
+ * The EqualCommand class represents the equality comparison operation.
+ * It evaluates two nodes and returns 1.0 if they are equal, otherwise returns 0.0.
+ *
+ * @author [Your Name]
+ */
 public class EqualCommand implements Command {
 
+  /**
+   * The number of arguments this command requires.
+   */
   public static final int NUM_ARGS = 2;
 
+  /**
+   * Constructs an instance of EqualCommand with the given model state and listener.
+   *
+   * @param modelState the model state
+   * @param listener the listener for state change events
+   */
   public EqualCommand(ModelState modelState, SlogoListener listener) {
+    // Constructor body can be left empty if it doesn't perform any actions.
   }
 
+  /**
+   * Executes the equality comparison operation on the provided nodes.
+   *
+   * @param arguments a list of nodes representing values to be compared
+   * @return 1.0 if the values are equal, otherwise returns 0.0
+   * @throws InvocationTargetException if an error occurs during execution
+   * @throws IllegalAccessException if access is denied during execution
+   */
   @Override
   public double execute(List<Node> arguments)
       throws InvocationTargetException, IllegalAccessException {
@@ -21,5 +46,4 @@ public class EqualCommand implements Command {
     double arg2 = arguments.get(1).evaluate();
     return (arg1 == arg2) ? 1.0 : 0.0;
   }
-
 }
