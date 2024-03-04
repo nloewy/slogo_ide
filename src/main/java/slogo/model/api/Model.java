8a88f5a70eb47aa95539ef53cea91e65c91b63ad
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 11:26:18 -0500

    Model Java Doc

--- src/main/java/slogo/model/api/Model.java
+++ src/main/java/slogo/model/api/Model.java
@@ -11,19 +11,19 @@ public interface Model {
       throws ClassNotFoundException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException, NoSuchFieldException, InvalidTokenException;
 
   /**
-   * Opens a file dialog to load a new XML file.
+   * Opens a file dialog to load a new Slogo file.
    *
    * @param path: the path of the file to be loaded
    */
-  File loadXml(String path);
+  File loadSlogo(String path);
 
   /**
-   * Saves the current state of the model to an XML file. Saves the commands, variables, and turtle
+   * Saves the current state of the model to a Slogo file. Saves the commands, variables, and turtle
    * state.
    *
    * @param path
    */
-  File saveXml(String path);
+  File saveSlogo(String path);
 
   /**
    * Resets the model to its initial state.
