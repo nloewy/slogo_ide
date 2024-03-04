97a4c3905186939ad8c85996132d024c4b14ae6a
Noah Loewy <noah.loewy@duke.edu>
2024-03-04 10:40:19 -0500

    Exceptions Javadoc

--- src/main/java/slogo/model/exceptions/InvalidOperandException.java
+++ src/main/java/slogo/model/exceptions/InvalidOperandException.java
@@ -1,5 +1,12 @@
 package slogo.model.exceptions;
 
+/**
+ * The InvalidOperandException is a runtime exception that occurs when a command attempts to perform
+ * an operation with operands that result in undefined behavior or are not supported by the
+ * operation's logic. This exception is typically thrown when a command leads to an undefined
+ * operation, such as dividing by zero or taking the square root of a negative number.
+ */
+
 public class InvalidOperandException extends RuntimeException {
 
   public InvalidOperandException(String s) {
--- src/main/java/slogo/model/exceptions/InvalidTokenException.java
+++ src/main/java/slogo/model/exceptions/InvalidTokenException.java
@@ -2,6 +2,13 @@ package slogo.model.exceptions;
 
 import slogo.model.api.SlogoException;
 
+/**
+ * The InvalidTokenException is a runtime exception that occurs when a token entered by the user
+ * does not match the regular expression pattern of any recognized token type as defined in the
+ * syntax configuration file (Syntax.properties). This exception typically arises during parsing /
+ * tokenization
+ */
+
 public class InvalidTokenException extends SlogoException {
 
   public InvalidTokenException(String s, String token) {
