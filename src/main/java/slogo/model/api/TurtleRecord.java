8dbb344f0866c9f4673ec5aaa75035a5683cf9b4
Noah Loewy <noah.loewy@duke.edu>
2024-03-05 21:01:06 -0500

    Changed API :)

--- src/main/java/slogo/model/api/TurtleRecord.java
+++ src/main/java/slogo/model/api/TurtleRecord.java
@@ -1,6 +1,6 @@
 package slogo.model.api;
 
 public record TurtleRecord(int id, double x, double y, boolean pen, boolean visible,
-                           double heading) {
+                           double heading, int bgIndex, int penColorIndex ) {
 
 }
