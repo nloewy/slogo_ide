1adfc62e83bb9b4bd5880a01c732c1efefdf6e08
Noah Loewy <noah.loewy@duke.edu>
2024-03-06 10:35:49 -0500

    Invalid RGB Color Error in English and Spanish

--- src/main/java/slogo/model/exceptions/InvalidRandomRangeInputException.java
+++ src/main/java/slogo/model/exceptions/InvalidRandomRangeInputException.java
@@ -1,7 +1,5 @@
 package slogo.model.exceptions;
 
-import slogo.model.exceptions.InvalidOperandException;
-
 public class InvalidRandomRangeInputException extends InvalidOperandException {
 
   public InvalidRandomRangeInputException(String s) {
