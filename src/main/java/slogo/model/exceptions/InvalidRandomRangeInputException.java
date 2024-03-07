5ad8e9d80648d1a4b6f711e473e2a85da674f6d1
Noah Loewy <noah.loewy@duke.edu>
2024-03-07 11:46:02 -0500

    Javadoc Comments for Exceptions Pt1

--- src/main/java/slogo/model/exceptions/InvalidRandomRangeInputException.java
+++ src/main/java/slogo/model/exceptions/InvalidRandomRangeInputException.java
@@ -1,5 +1,14 @@
 package slogo.model.exceptions;
 
+/**
+ * The InvalidRandomRangeInput class represents an exception that is thrown when attempting to
+ * select a random number between inputs MIN and MAX. If the input MAX exceeds MIN, this exception
+ * is thrown. This occurs during evaluation step of paring. It extends the InvalidOperandException
+ * class.
+ *
+ * @author Noah Loewy
+ */
+
 public class InvalidRandomRangeInputException extends InvalidOperandException {
 
   public InvalidRandomRangeInputException(String s) {
