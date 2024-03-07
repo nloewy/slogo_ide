210d20617712652a15d92a13f3fe9365611b47cf
Abhishek Chataut <ac802@duke.edu>
2024-03-07 02:12:03 -0500

    start screen turtle testing

--- src/main/java/slogo/view/pages/MainScreen.java
+++ src/main/java/slogo/view/pages/MainScreen.java
@@ -167,6 +167,7 @@ public class MainScreen implements SlogoListener {
     stage.show();
     centerX = centerPane.getBoundsInParent().getWidth() / 2;
     centerY = centerPane.getBoundsInParent().getHeight() / 2;
+    centerPane.setId("CenterPane");
     turtles.add(new FrontEndTurtle(1, centerX, centerY, Color.BLUE, true, 0, defaultImage, this));
 
     initializeTurtleDisplays();
@@ -322,6 +323,7 @@ public class MainScreen implements SlogoListener {
     createSpeedSlider();
 
     field = new TextField();
+    field.setId("CommandField");
     field.setPromptText(myResources.getString("EnterCommand"));
     field.setPrefSize(WINDOW_WIDTH - 1200, 300);
 
