dbecae3febf8f9696dfd633e84e3bf50627624e1
Noah Loewy <noah.loewy@duke.edu>
2024-03-07 16:20:22 -0500

    Push pre lab

--- src/main/java/slogo/model/command/multiple/AskCommand.java
+++ src/main/java/slogo/model/command/multiple/AskCommand.java
@@ -10,11 +10,10 @@ import slogo.model.node.Node;
 
 /**
  * The AskCommand class represents a command that given a turtle id(s), marks them as active for a
- * sequence of commands, then goes back to the previously active turtles.
+ * sequence of commands, then goes back to the previously active turtles
  *
  * @author Noah Loewy
  */
-
 public class AskCommand implements Command {
 
   /**
@@ -31,7 +30,6 @@ public class AskCommand implements Command {
    * @param modelState the model state
    * @param listener   the listener for state change events
    */
-
   public AskCommand(ModelState modelState, SlogoListener listener) {
     this.modelState = modelState;
     myListener = listener;
@@ -50,13 +48,24 @@ public class AskCommand implements Command {
   public double execute(List<Node> arguments, int turtleId) {
     List<Integer> tempList = new ArrayList<>();
     if (arguments.get(0).getChildren().isEmpty()) {
-      Node nextValueFound = arguments.get(0);
-      int id = (int) Math.round(nextValueFound.evaluate());
-      addToTempList(id, tempList);
+      int id = (int) Math.round(arguments.get(0).evaluate());
+      if (!modelState.getTurtles().containsKey(id)) {
+        modelState.getTurtles().put(id, new Turtle(id));
+        myListener.onUpdateTurtleState(modelState.getTurtles().get(id).getImmutableTurtle());
+      }
+      tempList.add(id);
     } else {
       for (Node node : arguments.get(0).getChildren()) {
+        if (node.getToken().equals("]")) {
+          continue;
+        }
         modelState.setOuter(false);
-        addToTempList((int) Math.round(node.evaluate()), tempList);
+        int id = (int) Math.round(node.evaluate());
+        if (!modelState.getTurtles().containsKey(id)) {
+          modelState.getTurtles().put(id, new Turtle(id));
+          myListener.onUpdateTurtleState(modelState.getTurtles().get(id).getImmutableTurtle());
+        }
+        tempList.add(id);
       }
     }
     modelState.getActiveTurtles().add(tempList);
@@ -67,23 +76,12 @@ public class AskCommand implements Command {
       modelState.setCurrTurtle(i);
       val = arguments.get(1).evaluate();
     }
+
     modelState.getActiveTurtles().pop();
     modelState.setOuter(false);
     return val;
 
   }
-
-  private void addToTempList(int id, List<Integer> tempList) {
-    if (!modelState.getTurtles().containsKey(id)) {
-      makeTurtleTellListener(id);
-      tempList.add(id);
-    }
-  }
-
-  private void makeTurtleTellListener(int id) {
-    modelState.getTurtles().put(id, new Turtle(id));
-    myListener.onUpdateTurtleState(modelState.getTurtles().get(id).getImmutableTurtle());
-  }
-
 }
+
 
