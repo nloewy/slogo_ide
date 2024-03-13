e895bc35ee00520e63221c925138c3a79953d076
Noah Loewy <noah.loewy@duke.edu>
2024-03-13 12:00:14 -0400

    POST-DEADLINE : Not sure if the APIs in plan were supposed to be updated, so heres the updated copies for the model one

--- doc/plan/api/ModelExternal.java
+++ doc/plan/api/ModelExternal.java
@@ -8,51 +8,9 @@ public interface ModelExternal {
   public void parse(String commandStr);
 
   /**
-   * Opens a file dialog to load a new XML file.
-   * @param path: the path of the file to be loaded
-   */
-  public File loadXml(String path);
-
-  /**
-   * Saves the current state of the model to an XML file.
-   * Saves the commands, variables, and turtle state.
-   * @param path
-   */
-  public File saveXml(String path);
-
-  /**
    * Resets the model to its initial state.
    */
   public void resetModel();
 }
 
 
-public interface SlogoListener {
-
-  /**
-   * Called when a variable value is updated.
-   *
-   * @param variableName The name of the variable.
-   */
-  void onUpdateValue(String variableName, Number newValue);
-
-  /**
-   * Called by a command that involves updating the state of the turtle
-   *
-   * @param turtleState The new state of the turtle.
-   */
-  void onUpdateTurtleState(TurtleRecord turtleState);
-
-  /**
-   * Called by a command that involves resetting a turtle, given its id
-   *
-   * @param id The turtle id of the reset turtle.
-   */
-  void onResetTurtle(int id);
-
-  /**
-   * Returns the result of a nested method call
-   * @param value of the outermost command in parse
-   */
-  void onReturn(double value);
-}
